4a91a2adb70c3f0afd178d842f712ab03fabb6aa
Owain Lewis <owain@owainlewis.com>
2014-12-18 13:31:34 +0000

    updates readme with the correct url mappings

--- src/js/ui/list/item/news.js
+++ src/js/ui/list/item/news.js
@@ -18,10 +18,10 @@ define(['jquery', 'utils/pubsub', 'ui/list/item'],
 
         style = hasImage ? 'background-image: url(' +(this.data.images.fullsize) +')' : '';
 
-      html = 
+      html =
         '<div class="background-image" style="' +style +'"></div>'
         + '<div class="list-item-news column-wrap ' +(hasImage ? 'list-item-news-with-image' : '') +'">'
-        + '<div class="list-item-news-inner">' 
+        + '<div class="list-item-news-inner">'
         + '<h2><a href="' +this.data.shareUrl +'">' +this.data.shortName +'</a></h2>'
         + '<p class="info"><span>' +this.data.collectionName +'</span> | ' +timeAgo +'</p>'
         + '<p class="summary">' +this.data.summary +'</p>';
@@ -29,22 +29,20 @@ define(['jquery', 'utils/pubsub', 'ui/list/item'],
           html += '<div class="news-item-image"><img src="' +this.data.images.fullsize +'"/></div>';
         }
         html += '</div></div>';
-        
+
       return html;
     };
 
     ListItemNews.prototype.postActivateHook = function() {
       var message = '';
       if (
-        !this.topic || 
+        !this.topic ||
         (this.topic && this.topic.name && this.topic.name != this.data.collectionName)
       ) {
-        message = 'From ' +this.data.collectionName +' ';
+//        message = 'From ' +this.data.collectionName +' ';
       }
-      message += moment(this.data.lastUpdated).fromNow() +'. '
-        + this.data.name +'. '
-        + this.data.summary +'.';
-      pubsub.emitEvent('speech:speak', [message]);
+//      message += moment(this.data.lastUpdated).fromNow() +'. ' + this.data.name;
+//      pubsub.emitEvent('speech:speak', [message]);
 
       pubsub.addListener('voice:more', function() {
         window.location = _this.data.shareUrl;
